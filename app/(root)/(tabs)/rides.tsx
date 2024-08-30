36b3ec11adc02688b65f8abe3fce442ba744b248
yura-prots <yuraprots@gmail.com>
2024-08-30 11:17:02 +0300

    copied files from "uber" repo

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Rides = () => {
+  return (
+    <SafeAreaView>
+      <Text>Rides</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Rides;
---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { useAuth } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
+
+const Home = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />;
+  }
+
+  return <Redirect href="/(auth)/welcome" />;
+};
+
+export default Home;
