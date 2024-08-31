dfb00ae4383d52edf0ed5b36a695e03c67f85259
yura-prots <yuraprots@gmail.com>
2024-08-31 19:18:45 +0300

    Created Map

---
+++ components/Map.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Text, View } from "react-native";
+
+const Map = () => {
+  return (
+    <View>
+      <Text>Map</Text>
+    </View>
+  );
+};
+
+export default Map;
