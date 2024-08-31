be161d082f69aa682ab73e15bd125dcdb74ef579
yura-prots <yuraprots@gmail.com>
2024-08-31 16:01:02 +0300

    created RideCard

---
+++ components/RideCard.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable prettier/prettier */
+import { icons } from "@/constants";
+import { formatDate, formatTime } from "@/lib/utils";
+import { Ride } from "@/types/type";
+import { Image, Text, View } from "react-native";
+
+const RideCard = ({
+  ride: {
+    destination_longitude,
+    destination_latitude,
+    destination_address,
+    origin_address,
+    created_at,
+    ride_time,
+    driver,
+    payment_status,
+  },
+}: {
+  ride: Ride;
+}) => (
+  <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
+    <View className="flex flex-col items-center justify-center p-3">
+      <View className="flex flex-row items-center justify-between">
+        <Image
+          source={{
+            uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+          }}
+          className="w-[80px] h-[90px]"
+        />
+
+        <View className="flex flex-col mx-5 gap-y-5 flex-1">
+          <View className="flex flex-row items-center gap-x-2">
+            <Image source={icons.to} className="w-5 h-5" />
+            <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+              {origin_address}
+            </Text>
+          </View>
+
+          <View className="flex flex-row items-center gap-x-2">
+            <Image source={icons.point} className="w-5 h-5" />
+            <Text className="text-md font-JakartaMedium" numberOfLines={1}>
+              {destination_address}
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View className="flex flex-col w-full mt-5 bg-general-500 rounded-lg p-3 items-start justify-center">
+        <View className="flex flex-row items-center w-full justify-between mb-5">
+          <Text className="text-md font-JakartaMedium text-grey-500">
+            Data & Time
+          </Text>
+          <Text className="text-md font-JakartaMedium text-grey-500">
+            {formatDate(created_at)}, {formatTime(ride_time)}
+          </Text>
+        </View>
+        <View className="flex flex-row items-center w-full justify-between mb-5">
+          <Text className="text-md font-JakartaMedium text-grey-500">
+            Driver
+          </Text>
+          <Text className="text-md font-JakartaMedium text-grey-500">
+            {driver.first_name}, {driver.last_name}
+          </Text>
+        </View>
+        <View className="flex flex-row items-center w-full justify-between mb-5">
+          <Text className="text-md font-JakartaMedium text-grey-500">
+            Car Seats
+          </Text>
+          <Text className="text-md font-JakartaMedium text-grey-500">
+            {driver.car_seats}
+          </Text>
+        </View>
+        <View className="flex flex-row items-center w-full justify-between mb-5">
+          <Text className="text-md font-JakartaMedium text-grey-500">
+            Payment Status
+          </Text>
+          <Text
+            className={`text-md capitalize font-JakartaMedium text-grey-500 ${payment_status === "paid" ? "text-green-500" : "text-red-500"}`}
+          >
+            {payment_status}
+          </Text>
+        </View>
+      </View>
+    </View>
+    {/* <Text className="text-3xl">{driver.first_name}</Text> */}
+  </View>
+);
+
+export default RideCard;
