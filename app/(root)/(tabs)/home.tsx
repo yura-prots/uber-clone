55f310862995d93f61444291eaa3a87e183d74c6
yura-prots <yuraprots@gmail.com>
2024-09-01 09:48:53 +0300

    add Google Maps

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -4,8 +4,10 @@ import GoogleTextInput from "@/components/GoogleTextInput";
 import Map from "@/components/Map";
 import RideCard from "@/components/RideCard";
 import { icons, images } from "@/constants";
+import { useLocationStore } from "@/store";
 import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
+import { useEffect, useState } from "react";
 import {
   ActivityIndicator,
   FlatList,
@@ -15,6 +17,7 @@ import {
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import * as Location from "expo-location";
 
 const recentRides = [
   {
@@ -126,9 +129,39 @@ const recentRides = [
 export default function Page() {
   const { user } = useUser();
   const loading = true;
+  const { setUserLocation, setDestinationLocation } = useLocationStore();
+
+  const [hasPermissions, setHasPermissions] = useState(false);
 
   const handleSignOut = () => {};
   const handleDestinationPress = () => {};
+
+  useEffect(() => {
+    const requestLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        setHasPermissions(false);
+
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync();
+
+      const address = await Location.reverseGeocodeAsync({
+        latitude: location.coords?.latitude!,
+        longitude: location.coords?.longitude!,
+      });
+
+      setUserLocation({
+        latitude: location.coords?.latitude!,
+        longitude: location.coords?.longitude!,
+        address: `${address[0].name}, ${address[0].region}`,
+      });
+    };
+
+    requestLocation();
+  }, []);
 
   return (
     <SafeAreaView className="bg-general-500">
@@ -188,6 +221,10 @@ export default function Page() {
                 <Map />
               </View>
             </>
+
+            <Text className="text-xl font-JakartaBold mt-5 mb-5">
+              Recent Rides
+            </Text>
           </>
         )}
       />
--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,35 @@
 /* eslint-disable prettier/prettier */
-import { Text, View } from "react-native";
+import { useLocationStore } from "@/store";
+import { Text } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 const Map = () => {
+  const {
+    userLongitude,
+    userLatitude,
+    destinationLongitude,
+    destinationLatitude,
+  } = useLocationStore();
+
+  const region = calculateRegion({
+    userLongitude,
+    userLatitude,
+    destinationLongitude,
+    destinationLatitude,
+  });
+
   return (
-    <View>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
       <Text>Map</Text>
-    </View>
+    </MapView>
   );
 };
 
