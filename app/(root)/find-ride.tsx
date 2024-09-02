41a2302a4239d8e777524f92a73e6332bc97475d
yura-prots <yuraprots@gmail.com>
2024-09-02 16:21:17 +0300

    create find-ride route

--- app/(root)/find-ride.tsx
+++ app/(root)/find-ride.tsx
@@ -1,5 +1,10 @@
 /* eslint-disable prettier/prettier */
+import CustomButton from "@/components/CustomButton";
+import GoogleTextInput from "@/components/GoogleTextInput";
+import RideLayout from "@/components/RideLayout";
+import { icons } from "@/constants";
 import { useLocationStore } from "@/store";
+import { router } from "expo-router";
 import { Text, View } from "react-native";
 
 const FindRide = () => {
@@ -11,10 +16,35 @@ const FindRide = () => {
   } = useLocationStore();
 
   return (
-    <View>
-      <Text className="text-2xl">You are here: {userAddress}</Text>
-      <Text className="text-2xl">You are going to: {destinationAddress}</Text>
-    </View>
+    <RideLayout title="Ride" snapPoints={["85%"]}>
+      <View className="my-3">
+        <Text className="text-lg fontJakartaSemiBold mb-3">From</Text>
+        <GoogleTextInput
+          icon={icons.target}
+          initialLocation={userAddress!}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="#f5f5f5"
+          handlePress={(location) => setUserLocation(location)}
+        />
+      </View>
+
+      <View className="my-3">
+        <Text className="text-lg fontJakartaSemiBold mb-3">To</Text>
+        <GoogleTextInput
+          icon={icons.target}
+          initialLocation={destinationAddress!}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="transparent"
+          handlePress={(location) => setDestinationLocation(location)}
+        />
+      </View>
+
+      <CustomButton
+        title="Find now"
+        onPress={() => router.push("/(root)/confirm-ride")}
+        className="mt-5"
+      />
+    </RideLayout>
   );
 };
 
