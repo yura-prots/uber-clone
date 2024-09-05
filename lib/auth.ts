a22b45103d717db92e80ba010b3db7eabd217e74
yura-prots <yuraprots@gmail.com>
2024-09-05 21:55:59 +0300

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,7 @@
 /* eslint-disable prettier/prettier */
-
 import * as SecureStore from "expo-secure-store";
-
-// export interface TokenCache {
-//   getToken: (key: string) => Promise<string | undefined | null>;
-//   saveToken: (key: string, token: string) => Promise<void>;
-//   clearToken?: (key: string) => void;
-// }
+import * as Linking from "expo-linking";
+import { fetchAPI } from "./fetch";
 
 export const tokenCache = {
   async getToken(key: string) {
@@ -31,4 +26,51 @@ export const tokenCache = {
       return;
     }
   },
+};
+
+export const googleOAuth = async (startOAuthFlow: any) => {
+  try {
+    const { createdSessionId, signUp, setActive } = await startOAuthFlow({
+      redirectUrl: Linking.createURL("/(root)/(tabs)/home", {
+        scheme: "myapp",
+      }),
+    });
+
+    if (createdSessionId) {
+      if (setActive) {
+        await setActive!({ session: createdSessionId });
+
+        if (signUp.createdUserId) {
+          await fetchAPI("/(api)/user", {
+            method: "POST",
+            body: JSON.stringify({
+              name: `${signUp.firstName} ${signUp.lastName}`,
+              email: signUp.emailAddress,
+              clerkId: signUp.createdUserId,
+            }),
+          });
+        }
+
+        return {
+          success: true,
+          code: "success",
+          message: "You have successfully authenticated",
+        };
+      }
+    }
+
+    return {
+      success: false,
+      code: "success",
+      message: "An error occurred",
+    };
+  } catch (error: any) {
+    console.log(error);
+
+    return {
+      success: false,
+      code: error.code,
+      message: error?.errors[0]?.longMessage,
+    };
+  }
 };
