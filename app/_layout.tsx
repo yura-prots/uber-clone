153eb3732db57d3abc59ea59c173cf4cd5c53987
yura-prots <yuraprots@gmail.com>
2024-09-06 22:05:38 +0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,11 +6,14 @@ import { useEffect } from "react";
 import "react-native-reanimated";
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 import { tokenCache } from "@/lib/auth";
+import { LogBox } from "react-native";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+LogBox.ignoreLogs(["Warning: ..."]);
 
 export default function RootLayout() {
   const [loaded] = useFonts({
